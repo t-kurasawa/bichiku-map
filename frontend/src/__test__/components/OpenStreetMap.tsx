a7f79261b04462cc8ba0a9e2690f8148f30a2f65
t-kurasawa <t-kurasawa@nri-digital.jp>
2022-01-20 22:17:37 +0900

    bugfix

--- frontend/src/__test__/components/OpenStreetMap.tsx
+++ frontend/src/__test__/components/OpenStreetMap.tsx
@@ -1,24 +1,24 @@
 import { render, screen } from '@testing-library/react';
-import Stockpile from 'components/Stockpile';
-
 import { ThemeProvider } from '@mui/material/styles';
 import { store } from 'store';
 import theme from 'theme';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
+import OpenStreetMap from 'components/OpenStreetMap';
+
 describe('コンポーネントテスト', () => {
   const TestTarget = () => (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <BrowserRouter>
-          <Stockpile />
+          <OpenStreetMap />
         </BrowserRouter>
       </ThemeProvider>
     </Provider>
   );
 
-  test('Stockpile', () => {
+  test('OpenStreetMap', () => {
     render(<TestTarget />);
     const element = screen.getByText('防災備蓄リスト');
     expect(element).toBeTruthy();
--- frontend/src/__test__/pages/Dashboard.test.tsx
+++ frontend/src/__test__/pages/Dashboard.test.tsx
@@ -1,5 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import { ThemeProvider } from '@mui/material/styles';
+import { store } from 'store';
+import theme from 'theme';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
+import Dashboard from 'pages/Dashboard';
+
 describe('画面テスト', () => {
+  const TestTarget = () => (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Dashboard />
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
+  );
+
   test('Dashboard', () => {
-    expect(true).toBeTruthy();
+    render(<TestTarget />);
+    const element = screen.getByText('防災備蓄リスト');
+    expect(element).toBeTruthy();
   });
 });
