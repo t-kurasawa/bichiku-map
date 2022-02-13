ee72c88079d8f2147cdb67bf2ee7ee09bedee25a
t-kurasawa <t-kurasawa@nri-digital.jp>
2022-02-13 12:35:22 +0900

    bugfix

--- frontend/src/apis/evacuation-api.ts
+++ frontend/src/apis/evacuation-api.ts
@@ -1,13 +1,15 @@
 import axios from 'axios';
-const baseURL = process.env.REACT_APP_BASE_URL;
+// const baseURL = process.env.REACT_APP_BASE_URL;
 
 const fetchEvacuationCenter = async () => {
-  const url = baseURL + '/evacuation/center';
+  // const url = baseURL + '/evacuation/center';
+  const url = 'http://localhost:3000/evacuation/center';
   return axios.get(url);
 };
 
 const fetchEvacuationArea = () => {
-  const url = baseURL + '/evacuation/area';
+  // const url = baseURL + '/evacuation/area';
+  const url = 'http://localhost:3000/evacuation/area';
   return axios.get(url);
 };
 
