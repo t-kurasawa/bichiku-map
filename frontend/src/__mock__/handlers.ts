0cb0bd4b0edbc60f6d0a44a1a091b016f8d0809c
t-kurasawa <t-kurasawa@nri-digital.jp>
2022-02-13 13:20:45 +0900

    bugifx import -> require

--- frontend/src/__mock__/handlers.ts
+++ frontend/src/__mock__/handlers.ts
@@ -5,11 +5,7 @@ import {
   EvacuationCenter,
   EvacuationArea,
 } from 'schema';
-// import stockpileType from './data/stockpileType.json';
-// import stockpileStatusEC from './data/stockpileStatusEC.json';
-// import evacuationCenter from './data/evacuationCenter.json';
-// import evacuationArea from './data/evacuationArea.json';
-
+// import ではホスティング時に動かないため require とする
 const stockpileType = require('./data/stockpileType.json');
 const stockpileStatusEC = require('./data/stockpileStatusEC.json');
 const evacuationCenter = require('./data/evacuationCenter.json');
